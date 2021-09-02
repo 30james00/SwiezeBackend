5753d643381e32ba951a70b8b5dd7148213d3263
30james00 <48206982+30james00@users.noreply.github.com>
2021-09-02 12:28:12 +0200

    Minor change in Seed.cs

--- Persistence/Seed.cs
+++ Persistence/Seed.cs
@@ -44,7 +44,7 @@ namespace Persistence
         
         private static Guid ToGuid(int value)
         {
-            byte[] bytes = new byte[16];
+            var bytes = new byte[16];
             BitConverter.GetBytes(value).CopyTo(bytes, 0);
             return new Guid(bytes);
         }
