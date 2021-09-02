fe603a465d2e94f67b5570c188c3b8d6d00c79d9
30james00 <48206982+30james00@users.noreply.github.com>
2021-09-02 11:48:02 +0200

    Add initial migration

---
+++ Persistence/Migrations/20210902094702_Init.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contacts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Mail = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    Phone = table.Column<string>(type: "character varying(13)", maxLength: 13, nullable: false),
+                    Voivodeship = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    PostalCode = table.Column<string>(type: "character varying(6)", maxLength: 6, nullable: false),
+                    City = table.Column<string>(type: "character varying(40)", maxLength: 40, nullable: false),
+                    Street = table.Column<string>(type: "character varying(120)", maxLength: 120, nullable: false),
+                    HouseNumber = table.Column<string>(type: "character varying(6)", maxLength: 6, nullable: false),
+                    FlatNumber = table.Column<string>(type: "character varying(4)", maxLength: 4, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacts", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contacts");
+        }
+    }
+}
