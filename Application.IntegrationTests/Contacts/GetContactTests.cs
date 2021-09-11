b4c0adba79ef8d10f6ae193a0ecf8e6d2fd12ebb
30james00 <48206982+30james00@users.noreply.github.com>
2021-09-11 13:53:26 +0200

    Add project for Integration Tests according to Clean Testing

---
+++ Application.IntegrationTests/Contacts/GetContactTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Application.IntegrationTests.Contacts
+{
+    public class GetContactTests
+    {
+        [Fact]
+        public void Success()
+        {
+            Assert.True(true);
+        }
+    }
+}
