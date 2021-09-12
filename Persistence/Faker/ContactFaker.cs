e101b162712ba59f5ce1e6a942bfe326c7a6a429
30james00 <48206982+30james00@users.noreply.github.com>
2021-09-12 17:48:39 +0200

    Add CreateWithId to ContactFaker.cs

--- Persistence/Faker/ContactFaker.cs
+++ Persistence/Faker/ContactFaker.cs
@@ -5,10 +5,9 @@ namespace Persistence.Faker
 {
     public class ContactFaker
     {
-        public static Faker<Contact> Create(int contactIndex)
+        public static Faker<Contact> Create()
         {
             return new Faker<Contact>()
-                .RuleFor(o => o.Id, f => GuidHelper.ToGuid(contactIndex++))
                 .RuleFor(o => o.Mail, f => f.Internet.Email())
                 .RuleFor(o => o.Phone, f => f.Random.Int(111111111, 999999999).ToString())
                 .RuleFor(o => o.Voivodeship, f => f.Address.State())
@@ -17,6 +16,12 @@ namespace Persistence.Faker
                 .RuleFor(o => o.Street, f => f.Address.StreetName())
                 .RuleFor(o => o.HouseNumber, f => f.Address.BuildingNumber())
                 .RuleFor(o => o.FlatNumber, f => $"{f.Random.String(1, 3, '0', '9')}{f.Random.String(0, 1, 'A', 'Z')}");
+        }
+
+        public static Faker<Contact> CreateWithId(int contactIndex)
+        {
+            return Create()
+                .RuleFor(o => o.Id, f => GuidHelper.ToGuid(contactIndex++));
         }
     }
 }
