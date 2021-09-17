77aa12c19ede9154296dd3ae2309136a43fc6e1f
30james00 <48206982+30james00@users.noreply.github.com>
2021-09-17 22:40:08 +0200

    Use custom validators in ContactValidator.cs

--- Application/Contacts/ContactValidator.cs
+++ Application/Contacts/ContactValidator.cs
@@ -1,4 +1,5 @@
 using System.Data;
+using Application.Core;
 using Domain;
 using FluentValidation;
 
@@ -9,13 +10,13 @@ namespace Application.Contacts
         public ContactValidator()
         {
             RuleFor(x => x.Mail).NotEmpty().MaximumLength(128).EmailAddress();
-            RuleFor(x => x.Phone).NotEmpty().MaximumLength(13);
+            RuleFor(x => x.Phone).NotEmpty().PhoneNumber();
             RuleFor(x => x.Voivodeship).NotEmpty().MaximumLength(20);
-            RuleFor(x => x.PostalCode).NotEmpty().MaximumLength(6);
+            RuleFor(x => x.PostalCode).NotEmpty().PostalCode();
             RuleFor(x => x.City).NotEmpty().MaximumLength(40);
             RuleFor(x => x.Street).NotEmpty().MaximumLength(120);
-            RuleFor(x => x.HouseNumber).NotEmpty().MaximumLength(6);
-            RuleFor(x => x.FlatNumber).MaximumLength(4);
+            RuleFor(x => x.HouseNumber).NotEmpty().HouseNumber();
+            RuleFor(x => x.FlatNumber).FlatNumber();
         }
     }
 }
