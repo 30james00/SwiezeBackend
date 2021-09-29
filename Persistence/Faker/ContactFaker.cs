b40c6013dbc57926c68fc90b7ae4c4baef12fed4
30james00 <48206982+30james00@users.noreply.github.com>
2021-09-29 14:54:05 +0200

    Fix phone in ContactFaker.cs

--- Persistence/Faker/ContactFaker.cs
+++ Persistence/Faker/ContactFaker.cs
@@ -9,7 +9,7 @@ namespace Persistence.Faker
         {
             return new Faker<Contact>()
                 .RuleFor(o => o.Mail, f => f.Internet.Email())
-                .RuleFor(o => o.Phone, f => f.Random.Int(111111111, 999999999).ToString())
+                .RuleFor(o => o.Phone, f => $"+48{f.Random.Int(111111111, 999999999).ToString()}")
                 .RuleFor(o => o.Voivodeship, f => f.Address.State())
                 .RuleFor(o => o.PostalCode, f => $"{f.Random.String(2, 2, '0', '9')}-{f.Random.String(3, 3, '0', '9')}")
                 .RuleFor(o => o.City, f => f.Address.City())
