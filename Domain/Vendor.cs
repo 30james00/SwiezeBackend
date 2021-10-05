49422827c86eda3f0ae239029f64dc9b879bae96
30james00 <48206982+30james00@users.noreply.github.com>
2021-10-05 20:22:24 +0200

    Minor nullable fix
    https://stackoverflow.com/questions/55492214/the-annotation-for-nullable-reference-types-should-only-be-used-in-code-within-a
    Caused by Github Action warnings

--- Domain/Vendor.cs
+++ Domain/Vendor.cs
@@ -8,8 +8,9 @@ namespace Domain
         public String Name { get; set; }
 
         public Account Account { get; set; }
+#nullable enable
         public string? AccountId { get; set; }
-
+#nullable disable
         public Contact Contact { get; set; }
     }
 }
