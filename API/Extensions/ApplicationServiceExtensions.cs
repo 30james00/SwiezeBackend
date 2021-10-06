4c4ff627effcc4c93a246f61eca2a2daee6bfe9e
30james00 <48206982+30james00@users.noreply.github.com>
2021-10-06 09:04:46 +0200

    Add UserAccessor.cs

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -1,5 +1,7 @@
 using Application;
 using Application.Core;
+using Application.Interfaces;
+using Infrastructure.Security;
 using MediatR;
 using MicroElements.Swashbuckle.FluentValidation.AspNetCore;
 using Microsoft.EntityFrameworkCore;
@@ -37,6 +39,7 @@ namespace API.Extensions
 
             services.AddMediatR(typeof(MediatREntrypoint).Assembly);
             services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+            services.AddScoped<IUserAccessor, UserAccessor>();
 
             return services;
         }
