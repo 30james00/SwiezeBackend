e8763aa1d2fe2c1d9df453daf4d44ea0abc6d417
30james00 <48206982+30james00@users.noreply.github.com>
2021-10-20 00:49:09 +0200

    Add predictable id to ProductFaker.cs

--- Persistence/Faker/ProductFaker.cs
+++ Persistence/Faker/ProductFaker.cs
@@ -6,12 +6,15 @@ namespace Persistence.Faker
 {
     public class ProductFaker
     {
+        private static int index = 1;
+
         public static Faker<Product> Create(List<UnitType> unitTypes, List<Vendor> vendors)
         {
             return new Faker<Product>()
+                .RuleFor(x => x.Id, _ => GuidHelper.ToGuid(index++))
                 .RuleFor(x => x.Name, f => f.Commerce.Product())
                 .RuleFor(x => x.Value, f => f.Random.Int(100, 50000))
-                .RuleFor(x => x.Unit, f => f.Random.Int(1,100000))
+                .RuleFor(x => x.Unit, f => f.Random.Int(1, 100000))
                 .RuleFor(x => x.Stock, f => f.Random.Int(1, 1000))
                 .RuleFor(x => x.UnitTypeId, f => f.PickRandom(unitTypes).Id)
                 .RuleFor(x => x.VendorId, f => f.PickRandom(vendors).Id);
