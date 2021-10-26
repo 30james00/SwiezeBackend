8ede05b36495346f165907d07737ced28900187a
30james00 <48206982+30james00@users.noreply.github.com>
2021-10-26 10:28:22 +0200

    Add CategoriesController.cs

---
+++ API/Controllers/CategoriesController.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Threading.Tasks;
+using Application.Categories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class CategoriesController : BaseApiController
+    {
+        [AllowAnonymous]
+        [HttpGet("{id:guid}")]
+        public async Task<IActionResult> DetailCategory(Guid id)
+        {
+            return HandleResult(await Mediator.Send(new DetailCategoryQuery(id)));
+        }
+    }
+}
